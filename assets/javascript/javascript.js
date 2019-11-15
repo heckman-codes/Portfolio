745110fb8d820e098ad01cb5000889462005345d
heckman-codes <liamheckmancode@gmail.com>
2019-11-14 19:39:17 -0800

    more polished portfolio site with icons.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,27 @@
+
+
+//
+M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems, options);
+});
+
+$(document).ready(function () {
+    $('.materialboxed').materialbox();
+});
+
+
+var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+});
+
+$(document).ready(function () {
+    $('.slider').slider();
+});
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
